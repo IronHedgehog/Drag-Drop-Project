5a373d8446da70a64b203fbee9503690412f6b9a
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-07-03 10:13:04 +0300

    autobind decorator + validation submited values

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+// autoBind decorator
+
+function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const methodUpdate: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const bindFN = originalMethod.bind(this);
+      return bindFN;
+    },
+  };
+  return methodUpdate;
+}
+
+// class ProjectInput
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -39,13 +54,33 @@ class ProjectInput {
     this.attach();
   }
 
+  private userInputValues(): [string, string, number] | void {
+    const titleInputValue = this.titleInputElement.value;
+    const descriptionInputValue = this.descriptionInputElement.value;
+    const peopleInputValue = this.peopleInputElement.value;
+
+    if (
+      titleInputValue.trim().length === 0 ||
+      descriptionInputValue.trim().length === 0 ||
+      peopleInputValue.trim().length === 0
+    ) {
+      alert("Invalid input, please try again");
+    }
+    return [titleInputValue, descriptionInputValue, +peopleInputValue];
+  }
+  // submit event
+  @autoBind
   private submitHandler(e: Event) {
     e.preventDefault();
-    console.log(this.titleInputElement.value);
+    const inputValues = this.userInputValues();
+    if (Array.isArray(inputValues)) {
+      const [title, desc, people] = inputValues;
+      console.log(title, desc, people);
+    }
   }
-
+  // method to add EventListener on the form
   private configureForm() {
-    this.formElement.addEventListener("submit", this.submitHandler.bind(this));
+    this.formElement.addEventListener("submit", this.submitHandler);
   }
   // method to add form at the screen
   private attach() {
--- src/func.ts
+++ src/func.ts
@@ -1,68 +1,68 @@
 // code on func(my)
 
-const refs = {
-  templateElement: document.getElementById(
-    "project-input"
-  )! as HTMLTemplateElement,
-  hostElement: document.getElementById("app")! as HTMLDivElement,
-};
+// const refs = {
+//   templateElement: document.getElementById(
+//     "project-input"
+//   )! as HTMLTemplateElement,
+//   hostElement: document.getElementById("app")! as HTMLDivElement,
+// };
 
-function projAddElements() {
-  const nodesFromTemplate = document.importNode(
-    refs.templateElement.content,
-    true
-  );
+// function projAddElements() {
+//   const nodesFromTemplate = document.importNode(
+//     refs.templateElement.content,
+//     true
+//   );
 
-  const formElement = nodesFromTemplate.firstElementChild! as HTMLFormElement;
+//   const formElement = nodesFromTemplate.firstElementChild! as HTMLFormElement;
 
-  const titleInputElement = formElement.querySelector(
-    "#title"
-  )! as HTMLInputElement;
-  const describeInputElement = formElement.querySelector(
-    "#description"
-  )! as HTMLInputElement;
-  const peopleInputElement = formElement.querySelector(
-    "#people"
-  )! as HTMLInputElement;
+//   const titleInputElement = formElement.querySelector(
+//     "#title"
+//   )! as HTMLInputElement;
+//   const describeInputElement = formElement.querySelector(
+//     "#description"
+//   )! as HTMLInputElement;
+//   const peopleInputElement = formElement.querySelector(
+//     "#people"
+//   )! as HTMLInputElement;
 
-  configForm(
-    formElement,
-    titleInputElement,
-    describeInputElement,
-    peopleInputElement
-  );
-  addToHTML(formElement);
-}
-const submitHandler = (
-  e: Event,
-  titleInputElement: HTMLInputElement,
-  describeInputElement: HTMLInputElement,
-  peopleInputElement: HTMLInputElement
-) => {
-  e.preventDefault();
-  console.log(titleInputElement.value);
-  console.log(describeInputElement.value);
-  console.log(peopleInputElement.value);
-};
+//   configForm(
+//     formElement,
+//     titleInputElement,
+//     describeInputElement,
+//     peopleInputElement
+//   );
+//   addToHTML(formElement);
+// }
+// const submitHandler = (
+//   e: Event,
+//   titleInputElement: HTMLInputElement,
+//   describeInputElement: HTMLInputElement,
+//   peopleInputElement: HTMLInputElement
+// ) => {
+//   e.preventDefault();
+//   console.log(titleInputElement.value);
+//   console.log(describeInputElement.value);
+//   console.log(peopleInputElement.value);
+// };
 
-const configForm = (
-  form: HTMLFormElement,
-  titleInputElement: HTMLInputElement,
-  describeInputElement: HTMLInputElement,
-  peopleInputElement: HTMLInputElement
-) => {
-  form.addEventListener("submit", (e) =>
-    submitHandler(
-      e,
-      titleInputElement,
-      describeInputElement,
-      peopleInputElement
-    )
-  );
-};
+// const configForm = (
+//   form: HTMLFormElement,
+//   titleInputElement: HTMLInputElement,
+//   describeInputElement: HTMLInputElement,
+//   peopleInputElement: HTMLInputElement
+// ) => {
+//   form.addEventListener("submit", (e) =>
+//     submitHandler(
+//       e,
+//       titleInputElement,
+//       describeInputElement,
+//       peopleInputElement
+//     )
+//   );
+// };
 
-const addToHTML = <T extends HTMLElement>(element: T) => {
-  refs.hostElement.insertAdjacentElement("afterend", element);
-};
+// const addToHTML = <T extends HTMLElement>(element: T) => {
+//   refs.hostElement.insertAdjacentElement("afterend", element);
+// };
 
-projAddElements();
+// projAddElements();
